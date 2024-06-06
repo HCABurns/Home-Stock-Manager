3673da12c2decad3ff6cd7ca5c6d3603ac0c833b
HCABurns <harrycaburns@gmail.com>
2024-06-06 17:07:27 +0100

    Fixed issue with database resetting on page change.
    The database does no long reset when changing from one page to another and instead is updated and displayed correctly.

--- app/src/main/java/database/dbHelper.java
+++ app/src/main/java/database/dbHelper.java
@@ -12,11 +12,21 @@ public class dbHelper {
 
     private ArrayList<Item> items = new ArrayList<>();
 
+    public dbHelper(){
+        setItems();
+    }
+
     /**
      * This function will return an ArrayList of Items retrieved from the database.
      * @return an ArrayList of Items that have been retrieved and put into an object.
      */
     public ArrayList<Item> getItems(){
+        return this.items;
+    }
+
+    public void setItems(){
+
+        //Todo: Get items fro database - Convert to item objects - Add to items arraylist.
         System.out.println("Getting items from database.");
         Item item = new Item("Cheese",2, Item.Type.OTHER,false);
         Item item2 = new Item("Apple",0, Item.Type.FRUIT,true);
@@ -28,7 +38,6 @@ public class dbHelper {
         items.add(new Item("Flour",0, Item.Type.OTHER,true));
         items.add(new Item("Egg",3, Item.Type.OTHER,false));
         items.add(new Item("Butter",1, Item.Type.OTHER,true));
-        return items;
     }
 
     public static void main(String[] args) {
