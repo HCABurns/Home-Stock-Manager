4419206631c85b9337aa860d2bace14ebd1fae55
Harry Burns <78239975+HCABurns@users.noreply.github.com>
2025-02-14 20:18:45 +0000

    Delete app/src/main/java/com/example/myapplication/database/DBInfo.java

--- app/src/main/java/com/example/myapplication/database/DBInfo.java
+++
@@ -1,9 +0,0 @@
-package com.example.myapplication.database;
-
-public class DBInfo {
-    public static String link = "https://stock-app-13e7a-default-rtdb.europe-west1.firebasedatabase.app";
-    public static String db_name = "items";
-    public static String db_name2 = "id_counter";
-    public static String db_name3 = "menu";
-
-}
